cf95007cee4aa6356e3e25c847096f24bf954bb2
SHUSTRIK <shustrikofficial.milan@gmail.com>
2021-03-20 13:11:35 +0300

    Проверка

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on('message', message => {
     }else{
 
     } */
-    console.log(streets.find(st => st.radius.find(st => st == message.channel.name)));
+    message.channel.send(`Соседняя улица: ${streets.find(st => st.radius.find(st => st == message.channel.name))}`);
 });
 
 client.login(process.env.BOT_TOKEN);
