d504d4d8e3d9f96f37ad25b71750434d01fda148
SHUSTRIK-Milan <shustrikofficial.milan@gmail.com>
2021-04-23 21:07:54 +0300

    💡 Очистка БД через команду

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ const BDpref = '^';
 const urlSteam = `https://steamcommunity.com/`;
 
 var guild;
-var BDchnl = `833225101218152459`;
-var dopBDmsg = `833260237481705502`;
+const BDchnl = `833225101218152459`;
+const dopBDmsg = `833260237481705502`;
 
 const SteamAPI = require('steamapi');
 const steam = new SteamAPI('52E6781CF3B4EB4234DC424555A7AD9C');
@@ -385,7 +385,16 @@ client.on('message', message => {
 
     if(comand(message).com == 'cbd'){
         message.delete()
-        message.channel
+        let channel = guild.channels.cache.get(BDchnl); //получаем канал в котором находится наша БД
+        channel.messages.fetch(dopBDmsg).then(oMsg => { //получаем сообщение доп бд
+            let nMsg = oMsg.content.split('\n'); //разделяем доп бд на строки
+            let fMsg = nMsg[parseInt(comand(message).sarg[0])-1].split(BDpref); //получаем последние данные в доп бд
+            if (fMsg[0] == ''){
+                fMsg.splice(0,1);
+            };
+            console.log(parseInt(comand(message).sarg[0])-1);
+            message.channel.send(`!edit 833225101218152459 ${fMsg[0]} > **БАЗА ДАННЫХ ПОЛЬЗОВАТЕЛЕЙ 1**`);
+        });
     };
 
     if(message.guild == undefined && mb == false){
