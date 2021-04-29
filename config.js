54e22f88b5f9a2f38591bd3488f9ffbbe2ce0616
SHUSTRIK <shustrikofficial.milan@gmail.com>
2021-04-29 12:05:24 +0300

    🌟 Занёс IDшники категорий в config.js. По прежнему работа над переносом каналов из объекта streets, содержащего в себе все улицы и объекты, в реалии дискорда.

--- config.js
+++ config.js
@@ -1,15 +1,24 @@
+module.exports.catID = {
+    main: `814795850885627965`, //main[ooc]
+    rules: `835433939812089856`, //projectRules
+    team_general: `822762670058766336`, //team-general
+    team_admin: `835628015986671626`, //team-admin
+    team_helper: `835204987285274624`, //team-helper
+    team_moders: `835205499397472296` //team-moders
+};
+
 module.exports.channelsID = {
-    logsId: `825078587312177162`,
-    commitsID: `829434075688206347`,
-    infoID: `822528060196388926`,
-    devID: `822796606952177664`,
-    mainIDusers: `822529113239453706`,
-    questID: `822885506270232651`,
-    mainIDteam: `822493674738941963`,
-    headsID: `831214097005281290`,
-    devTeamID: `829995884150390795`,
-    generalR: `835434150596837446`,
-    generalT: `835434387293339658`
+    logsId: `825078587312177162`,//logs
+    commitsID: `829434075688206347`,//commits
+    infoID: `822528060196388926`,//welcome
+    devID: `822796606952177664`,//devblog
+    mainIDusers: `822529113239453706`,//chat
+    questID: `822885506270232651`,//qa
+    mainIDteam: `822493674738941963`,//chat
+    headsID: `831214097005281290`,//heads
+    devTeamID: `829995884150390795`,//dev-team
+    generalR: `835434150596837446`,//general rules
+    generalT: `835434387293339658`//general terminology
 };
 
 module.exports.BLChannelsID = {
