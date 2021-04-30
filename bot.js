aadca07968ab84aeffd70a8477ac47860f913ce3
SHUSTRIK <shustrikofficial.milan@gmail.com>
2021-04-30 13:56:06 +0300

    🌟 Помещение функции !update, повлекшей за собой много ошибок, потраченных нервов и т.д, в архив.

--- bot.js
+++ bot.js
@@ -436,54 +436,6 @@ async function Stats(message){
     
 };
 
-async function updateChannels(){
-    let allChannels = guild.channels.cache;
-    let channelsID = [];
-    let pawStreets = [];
-    let pawObjects = [];
-
-    for (channel in Config.channelsID) channelsID.push(Config.channelsID[channel]);
-    for (outAllChannel of allChannels){
-        for(street of Config.streets) if(`«${street.name.toLowerCase()}»` == outAllChannel[1].name.toLowerCase()) channelsID.push(`${outAllChannel[0]}`)
-        for(street of Config.streets) if(street.objects.find(object => object.name.toLowerCase() == outAllChannel[1].name.toLowerCase()) != undefined) channelsID.push(`${outAllChannel[0]}`)
-    };
-
-    for (outAllChannel of allChannels){
-        let pawStreet = Config.streets.find(street => `«${street.name.toLowerCase()}»` == outAllChannel[1].name.toLowerCase());
-        if(pawStreet != undefined) pawStreets.push(pawStreet);
-        
-        for(street of Config.streets){
-            pawObject = street.objects.find(object => object.name.toLowerCase() == outAllChannel[1].name.toLowerCase());
-            if(pawObject != undefined) pawObjects.push(pawObject);
-        };
-
-        if(channelsID.find(channel => channel == outAllChannel[0]) == undefined){
-            guild.channels.cache.get(outAllChannel[0]).delete();
-        };
-        /* Мы перебираем все каналы и путём проверки на наличие данных отделяем те, которые есть в файли и которых нет.
-        То бишь, мы сравниваем каналы и те, которые ничему не равны удаляем.*/
-    };
-
-    let oStreets = Config.streets.filter(street => !pawStreets.includes(street));
-    let oObjects = [];
-    for(street of Config.streets){
-        fobjects = street.objects.filter(object => !pawObjects.includes(object));
-        for(fo of fobjects) oObjects.push(fo);
-    };
-
-    for (street of oStreets){
-        var cat = await guild.channels.create(`«${street.name}»`,{
-            type:'category', permissionOverwrites:[{id:`814795850885627964`,deny:'VIEW_CHANNEL'}]
-        });
-        
-        for(object of oObjects){
-            if(street.objects.find(obj => obj == object) != undefined) guild.channels.create(`${object.name}`,{
-                type:'text', parent:cat, permissionOverwrites:[{id:`814795850885627964`,deny:'VIEW_CHANNEL'}]
-            });
-        };
-    };
-};
-
 client.on('messageDelete', (message) => {
     sendLog(message,'Общее',`Сообщение удалено`,'Успешно',`Содержимое сообщения: ${message.content}`)
 });
