1187aa02f6b5d2d1fe0c1a74786fde1874358039
SHUSTRIK-Milan <shustrikofficial.milan@gmail.com>
2021-05-30 13:06:56 +0300

    🌟Загружаем на борт тестового бота discord.js

---
+++ test_bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const Config = require('./config');
+const client = new Discord.Client();
+
+var guild;
+const dopBDmsg = `840180165665619998`;
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} ready!`);
+    guild = client.guilds.cache.get('814795850885627964');
+});
+
+client.login(process.env.TestBOT_TOKEN);
