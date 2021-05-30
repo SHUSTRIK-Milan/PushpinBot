e6ed91699d1a9e790d7c02b1244ffbd01278a4e2
Eblyashine <vladislav.syrtsov@mail.ru>
2021-05-30 17:19:20 +0500

    Тестовый коммит

--- test_bot.js
+++ test_bot.js
@@ -1,16 +1,28 @@
-const Discord = require('discord.js');
+
+const Discord = require('discord.js')
 const Config = require('./config');
 const client = new Discord.Client();
 
 var guild;
 
+
 client.on('ready', () => {
     console.log(`${client.user.tag} ready!`);
     guild = client.guilds.cache.get('840180165665619998');
 });
+client.on("messageDelete", (message) => {
+    message.channel.send(message.author.username+' удалил сообщение: '+ message.content+message.channel.name)
+})
 
 client.on('message', (message) => {
     console.log(message.content);
+    if(message.content == 'dest' && !message.author.bot){
+        client.destroy();
+    
+    }
+    if(message.content.toLowerCase() == "cock" && !message.author.bot){
+        message.channel.send( ' Cock, please ')
+    }
 })
 
 client.login(Config.discordTocens.testBot);
