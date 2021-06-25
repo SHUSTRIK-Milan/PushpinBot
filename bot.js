ec32622ce5472cbead9af7a59a92950fbf0728b5
SHUSTRIK-Milan <shustrikofficial.milan@gmail.com>
2021-06-26 00:52:12 +0300

    💡 Снова фикс

--- bot.js
+++ bot.js
@@ -599,11 +599,11 @@ client.on('message', message => {
     let rpchannels = Object.values(Config.channelsID).filter(chl => chl == message.channel.id);
     
     if (Object.values(Config.logChannels).find(chl => chl == message.channel.id) != null){
-        setTimeout(() => {if (!mb && !mg) sendLog(message,`Общее`,`Отправил сообщение.`,`Успешно`,`${message.content}`)}, 300);
+        setTimeout(() => {if (!mb && !mg) sendLog(message,`Общее`,`Отправил сообщение.`,`Успешно`,`${message.content}`)}, 1000);
     }
     let logChannel = Object.values(Config.logChannels).find(chl => chl == message.channel.id)
     if (logChannel == null){
-        setTimeout(() => {if (!mb && !mg && rpchannels.find(chnl == logChannel) == null) sendLog(message,`РП`,`Отправил сообщение.`,`Успешно`,`${message.content}`)}, 300);
+        setTimeout(() => {if (!mb && !mg && rpchannels.find(chnl == logChannel) == null) sendLog(message,`РП`,`Отправил сообщение.`,`Успешно`,`${message.content}`)}, 1000);
     }
 
     if(message.content == '⠀' && message.author.bot){
