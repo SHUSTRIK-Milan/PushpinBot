193e7500ccb46370bebe295c5bf9165a20381dfd
SHUSTRIK-Milan <shustrikofficial.milan@gmail.com>
2021-07-11 14:06:56 +0300

    🌟 Теперь рофл-бот отправляет и картинки.

--- bot.js
+++ bot.js
@@ -643,6 +643,7 @@ async function roflBot(text, messageG){
             .then(message => {
                 msgs = message.map(message => message)
                 let ed = `${msg.content}\n${messageG.content}^${msgs[0].content}^<@!${msgs[0].author.id}>`
+                if(msgs[0].embeds.image != undefined) ed = `${msg.content}\n${messageG.content}^${msgs[0].content} ${msgs[0].embeds.image.url}^<@!${msgs[0].author.id}>`
 
                 if(ed.length < 1800){
                     messageG.channel.send('Спасибо!');
