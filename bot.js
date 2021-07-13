975e587ed497e7ca8c8f4e470de0ef4d91d59223
deadfladar <vladislav.syrtsov@mail.ru>
2021-07-13 23:21:35 +0500

    Абобус

--- bot.js
+++ bot.js
@@ -1954,3 +1954,4 @@ client.on('error', err => {
     console.log('Краш!')
     guild.channels.cache.get(Config.channelsID.serverMsg).send(`> Бот неожиданно отключился! Скорее оповестите SHUSTRIK'а, <@&833226140755689483> и <@&833227050550296576>`)
 }); */
+// Абобус
