ae22f0d1de4e1c43f90d71c02544b99e859540f8
SHUSTRIK-Milan <shustrikofficial.milan@gmail.com>
2021-07-14 15:56:32 +0300

    🌟 Тест вида предметов в инвентаре

--- bot.js
+++ bot.js
@@ -934,6 +934,26 @@ client.on('message', message => {
     if(message.channel.id == Config.channelsID.bot && !mb && !mg){
         roflBot(message.content, message)
     }
+
+    if(comand(message).com == `test` && head && !mb && !mg){
+        setTimeout(() => message.delete(), timeOfDelete);
+        message.channel.send({
+            content: "This is a message with components",
+            components: [
+                {
+                    type: 1,
+                    components: [
+                        {
+                            type: 2,
+                            label: "Click me!",
+                            style: 1,
+                            custom_id: "click_one"
+                        }
+                    ]
+                }
+            ]
+        })
+    }
 });
 
 const config = {
